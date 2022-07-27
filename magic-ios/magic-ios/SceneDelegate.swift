33d22254c07f872930ad79be258ff7fe7595d00d
e.de.farias.batista <e.de.farias.batista@accenture.com>
2022-07-27 09:40:45 -0300

    scene

--- magic-ios/magic-ios/SceneDelegate.swift
+++ magic-ios/magic-ios/SceneDelegate.swift
@@ -16,13 +16,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let viewController = UIViewController()
         
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window.windowScene = windowScene
-        window.rootViewController = UIViewController()
+        window.rootViewController = viewController
         window.makeKeyAndVisible()
-        
         self.window = window
     }
 
